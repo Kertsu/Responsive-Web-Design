8cca5d4c1eb14b2e4bb2e61c51177d72792799d7
Kertsu <kurtddbigtas@gmail.com>
2022-09-29 20:26:29 +0800

    update

---
+++ scripts/my-scripts.js
@@ -0,0 +1,9 @@
+
+// function showXButton() {
+//   document.getElementById("mobile-nav-toggle-x").style.display = "inline";
+//   document.getElementById("mobile-nav-toggle").style.display = "none";
+// }
+// function showMenu() {
+//   document.getElementById("mobile-nav-toggle").style.display = "inline";
+//   document.getElementById("mobile-nav-toggle-x").style.display = "none";
+// }
