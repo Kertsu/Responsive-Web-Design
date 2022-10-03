1eb4758d0e6d97da45fe66b0b174e0b617589bd8
Kertsu <kurtddbigtas@gmail.com>
2022-10-04 01:53:54 +0800

    update

--- scripts/my-scripts.js
+++ scripts/my-scripts.js
@@ -1,20 +1,12 @@
-// function showMenu(){
-//     var menu = document.getElementById("menu");
-//     var btn = document.getElementById("menu-btn");
-//     if (menu.style.width == 0){
-//         menu.style.width = "260px";
-//         btn.classList.toggle("fa-x")
-
-//     } else{
-//         menu.style.width = 0;
-//         btn.classList.toggle("")
-//     } 
-// }
-
-
 $(document).ready(function(){
         $('.button').click(function(){
             $('.menu-div').toggleClass('animate');
             $('.fas').toggleClass('xbutton');
         });
     });
+
+$(document).ready(function(){
+    $('.brand-name').click(function(){
+        window.location.href = 'index.html'
+    });
+});  
