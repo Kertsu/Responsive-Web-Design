7c5718cb82d87067347df0288b664d2931086863
Kertsu <kurtddbigtas@gmail.com>
2022-10-11 17:13:15 +0800

    updated

--- scripts/my-scripts.js
+++ scripts/my-scripts.js
@@ -5,8 +5,19 @@ $(document).ready(function(){
         });
     });
 
-// $(document).ready(function(){
-//     $('.brand-name').click(function(){
-//         window.location.href = 'index.html'
-//     });
-// });  
+$(document).ready(function(){
+    $('.fa-moon').click(function(){
+        $(this).toggleClass('light');
+    });
+});
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() <= 0){
+            $('#header').css('box-shadow', 'none') 
+        } else{
+           $('#header').css('box-shadow','-21.213px 21.213px 30px 0px rgb(158 158 158 / 30%)')
+        }
+    });
+    });
+
